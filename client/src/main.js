385b2de44b793be9aa78128a6b15cd8ec1c365e1
e <563798627@qq.com>
2019-01-07 22:14:34 +0800

    add pxtorem

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 import './assets/stylus/index.styl'
-
+import 'amfe-flexible/index.min'
 // Vue.config.productionTip = false
 
 fastclick.attach(document.body)
