6dc583c962e65e1b380c1fce078c52de657fbc0d
e <563798627@qq.com>
2019-01-14 09:43:11 +0800

    modify config

--- client/config/index.js
+++ client/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://m.ctrip.com/restapi/soa2',
         pathRewrite: {
           '^/api' : ''
         }
